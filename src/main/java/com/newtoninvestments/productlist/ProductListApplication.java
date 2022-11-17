1ce36a86b8b22c1fa1fa113a46e9f3d961e0da27
Yulia Suslova <suslova.dzhulia2017@yandex.ru>
2022-11-17 14:46:20 +0300

    Initial commit

---
+++ src/main/java/com/newtoninvestments/productlist/ProductListApplication.java
@@ -0,0 +1,13 @@
+package com.newtoninvestments.productlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductListApplication.class, args);
+	}
+
+}
